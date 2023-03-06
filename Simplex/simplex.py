4383148a0b75978bddfedb9d3fca44e42a64b4de
Dallin Seyfried <seyfdall@byu.edu>
2023-03-06 03:04:40 -0700

    Start Simplex

--- Simplex/simplex.py
+++ Simplex/simplex.py
@@ -29,7 +29,22 @@ class SimplexSolver(object):
         Raises:
             ValueError: if the given system is infeasible at the origin.
         """
-        return NotImplementedError("Problem 1 Incomplete")
+        # Setup initial length values
+        self.n = len(c)
+        self.m = len(b)
+
+        # Test origin feasibility
+        zeros_arr = np.zeros(self.n)
+        if np.min(A @ zeros_arr) > np.min(b):
+            raise ValueError(f"Given system is infeasible at the origin.")
+
+        # Set parameters as class attributes if feasible
+        self.c = c
+        self.A = A
+        self.b = b
+
+        # Generate dictionary
+        self._generatedictionary(self.c, self.A, self.b)
 
     # Problem 2
     def _generatedictionary(self, c, A, b):
@@ -40,28 +55,61 @@ class SimplexSolver(object):
             A ((m,n) ndarray): The constraint coefficients matrix.
             b ((m,) ndarray): The constraint vector.
         """
-        raise NotImplementedError("Problem 2 Incomplete")
+        # Create components of the D matrix
+        A_bar = np.column_stack((A, np.eye(self.m)))
+        c_bar = np.append(c, np.zeros(self.m))
+
+        # Construct the D matrix
+        D_top = np.insert(c_bar, 0, 0)
+        D_bottom = np.column_stack((b, -A_bar))
+        D = np.row_stack((D_top, D_bottom))
+
+        self.D = D
 
 
     # Problem 3a
     def _pivot_col(self):
         """Return the column index of the next pivot column.
         """
-        return NotImplementedError("Problem 3 Incomplete")
+        # Find the column index
+        col_ind = np.where(self.D[0, 1:] < 0)[0]
+        return col_ind[0] + 1
 
     # Problem 3b
     def _pivot_row(self, index):
         """Determine the row index of the next pivot row using the ratio test
         (Bland's Rule).
         """
-        return NotImplementedError("Problem 3 Incomplete")
+        # Calculate which rows are valid - if none then problem is unbounded
+        neg_rows = np.insert(self.D[1:, index] < 0, 0, False)
+        if len(neg_rows) == 0:
+            raise ValueError("Problem is unbounded")
+
+        # Find the appropriate row_index/indices
+        row_ind = np.argmin(np.abs(self.D[neg_rows, 0] / self.D[neg_rows, index]))
+
+        # Blanch's rule to return first indexed
+        if type(row_ind) is np.ndarray:
+            row_ind = row_ind[0]
+
+        return np.where(neg_rows != 0)[0][row_ind]
 
     # Problem 4
     def pivot(self):
         """Select the column and row to pivot on. Reduce the column to a
         negative elementary vector.
         """
-        return NotImplementedError("Problem 4 Incomplete")
+        # Find where pivot is in array
+        col_index = self._pivot_col()
+        row_index = self._pivot_row(col_index)
+
+        # Divide pivot row by abs(pivot)
+        self.D[row_index, :] = self.D[row_index, :] / np.abs(self.D[row_index, col_index])
+
+        # Simplify matrix by zeroing out pivot column with row operations
+        for i in range(len(self.D)):
+            if i != row_index:
+                self.D[i, :] = self.D[i, :] - self.D[i, col_index] / self.D[row_index, col_index] * self.D[row_index, :]
 
     # Problem 5
     def solve(self):
@@ -72,7 +120,37 @@ class SimplexSolver(object):
             (dict): The basic variables and their values.
             (dict): The nonbasic variables and their values.
         """
-        raise NotImplementedError("Problem 5 Incomplete")
+        # Pivot and cycle through the dictionary until solved or unbounded error found
+        while True in (self.D[0, 1:] < 0):
+            self.pivot()
+
+        # Calculate min
+        min = self.D[0, 0]
+        independent = dict()
+        dependent = dict()
+
+        # Create dictionaries for independent and dependent variables
+        for i in range(1, len(self.D[0])):
+            if self.D[0, i] == 0:
+                dependent[i - 1] = (self.D[:, i] == -1) @ (self.D[:, 0])
+            else:
+                independent[i - 1] = 0
+
+        return tuple((min, dependent, independent))
+
+
+# Test Simplex Class
+def test_simplex_solver():
+    # Test Simplex Solver Class
+    # Initialize objective function and constraints.
+    c = np.array([-3., -2.])
+    b = np.array([2., 5, 7])
+    A = np.array([[1., -1], [3, 1], [4, 3]])
+    # Instantiate the simplex solver, then solve the problem.
+    solver = SimplexSolver(c, A, b)
+    sol = solver.solve()
+    print(sol)
+
 
 # Problem 6
 def prob6(filename='productMix.npz'):
@@ -84,4 +162,14 @@ def prob6(filename='productMix.npz'):
     Returns:
         ((n,) ndarray): the number of units that should be produced for each product.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    data = np.load(filename)
+    resource_coeffs = data.f.A
+    unit_prices = data.f.p
+    available_resource_units = data.f.m
+    demand_constraints = data.f.d
+    pass
+
+
+# Test Problem 6
+def test_prob6():
+    prob6()
