810cfd43d50738aa118c4c295d7a0ccfffba1efa
Dallin Seyfried <seyfdall@byu.edu>
2023-04-02 03:11:11 -0600

    Start interior linear lab

--- InteriorPoint_Linear/interior_point_linear.py
+++ InteriorPoint_Linear/interior_point_linear.py
@@ -1,8 +1,8 @@
 # interior_point_linear.py
 """Volume 2: Interior Point for Linear Programs.
-<Name>
-<Class>
-<Date>
+<Name> Dallin Seyfried
+<Class> 001
+<Date> 4/1/2023
 """
 
 import numpy as np
@@ -80,9 +80,129 @@ def interiorPoint(A, b, c, niter=20, tol=1e-16, verbose=False):
         x ((n, ) ndarray): The optimal point.
         val (float): The minimum value of the objective function.
     """
-    raise NotImplementedError("Problems 1-4 Incomplete")
+    n = len(c)
+    m = len(b)
+
+    # Define F
+    def F(x, lam, mu):
+        return np.hstack((A.T @ lam + mu - c, A @ x - b, np.diag(mu) @ x))
+
+    # Do a linear search to find next step
+    def search_direction(x, lam, mu):
+        # Find the derivative of F
+        top_row = np.hstack((np.zeros((n, n)), A.T, np.eye(n)))
+        mid_row = np.hstack((A, np.zeros((m, m)), np.zeros((m, n))))
+        bot_row = np.hstack((np.diag(mu), np.zeros((n, m)), np.diag(x)))
+        A_mat = np.vstack((top_row, mid_row, bot_row))
+
+        # Calculate the right hand side
+        v = x @ mu / n
+        b_vec = -F(x, lam, mu) + np.concatenate((np.zeros(n + m), np.ones(n) * v / 10))
+
+        # lu_factor A_mat to get compatible with lu_solve
+        lu, piv = la.lu_factor(A_mat)
+
+        # lu_solve for deltas
+        search_dir = la.lu_solve((lu, piv), b_vec)
+        return search_dir
+
+    # Calculate step size
+    def step_size(x, mu, deltas):
+        # Get specific x and mu deltas
+        x_deltas = deltas[:n]
+        mu_deltas = deltas[n+m:]
+
+        # Calculate maxes
+        mu_mask = mu_deltas < 0
+        x_mask = x_deltas < 0
+        alpha_max = 1 if np.sum(mu_mask) == 0 else np.min(-mu[mu_mask] / mu_deltas[mu_mask])
+        x_max = 1 if np.sum(x_mask) == 0 else np.min(-x[x_mask] / x_deltas[x_mask])
+
+        # Calculate alpha and delta
+        alpha = min(1, 0.95 * alpha_max)
+        delta = min(1, 0.95 * x_max)
+        return alpha, delta
+
+    # Get the starting point
+    x, lam, mu = starting_point(A, b, c)
+
+    # Cycle niter times to get optimum and optimizer
+    for i in range(niter):
+        # Compute search direction
+        direction = search_direction(x, lam, mu)
+
+        # Compute step size
+        alpha, delta = step_size(x, mu, direction)
+
+        # Take the step
+        x = x + delta * x
+        lam = lam + alpha * lam
+        mu = mu + alpha * mu
+
+        # Check if optimal
+        if x @ mu / n < tol:
+            break
+
+    return x, c @ x
+
+
+# Test interior point
+def test_interior_point():
+    j, k = 7, 5
+    A, b, c, x = randomLP(j, k)
+    point, value = interiorPoint(A, b, c)
+    print('\n')
+    print(x)
+    print(point[:k])
+    print(np.allclose(x, point[:k]))
+    # A, b, c_neg, x = randomLP(2, 2)
+    # opt, val = interiorPoint(A, b, -c_neg)
 
 
 def leastAbsoluteDeviations(filename='simdata.txt'):
     """Generate and show the plot requested in the lab."""
-    raise NotImplementedError("Problem 5 Incomplete")
+    # Load data
+    data = np.loadtxt('simdata.txt')
+    m = data.shape[0]
+    n = data.shape[1] - 1
+
+    # Initialize vectors c, y, and x
+    c = np.zeros(3 * m + 2 * (n + 1))
+    c[:m] = 1
+    y = np.empty(2*m)
+    y[::2] = -data[:, 0]
+    y[1::2] = data[:, 0]
+    x = data[:, 1:]
+
+    # Create the A matrix
+    A = np.ones((2 * m, 3 * m + 2 * (n + 1)))
+    A[::2, :m] = np.eye(m)
+    A[1::2, :m] = np.eye(m)
+    A[::2, m:m + n] = -x
+    A[1::2, m:m + n] = x
+    A[::2, m + n:m + 2 * n] = x
+    A[1::2, m + n:m + 2 * n] = -x
+    A[::2, m + 2 * n] = -1
+    A[1::2, m + 2 * n + 1] = -1
+    A[:, m + 2 * n + 2:] = -np.eye(2 * m, 2 * m)
+
+    # Get the solution point
+    sol = interiorPoint(A, y, c, niter=10)[0]
+
+    # Get beta and the b vector
+    beta = sol[m: m + n] - sol[m + n: m + 2 * n]
+    b = sol[m + 2 * n] - sol[m + 2 * n + 1]
+
+    # Plot least squares solution to compare results
+    slope, intercept = linregress(data[:, 1], data[:, 0])[:2]
+    domain = np.linspace(0, 10, 200)
+    plt.plot(domain, domain * slope + intercept, label="Least Squares")
+    plt.plot(domain, domain * beta + b, label="LAD")
+    plt.tight_layout()
+    plt.show()
+    pass
+
+
+# Test leastAbsoluteDeviations
+def test_LAD():
+    leastAbsoluteDeviations()
